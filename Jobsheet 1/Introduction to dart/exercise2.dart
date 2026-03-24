a5598e28e1200698ddefcc0e580665211012c6e6
Maria Aulia <72290963+mariaulia@users.noreply.github.com>
2026-03-24 17:26:26 +0700

    Create exercise2.dart

---
+++ Jobsheet 1/Introduction to dart/exercise2.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  int numberToCheck = 13;
+    
+    if (numberToCheck % 2 == 0) {
+    print('$numberToCheck is Even (Genap)');
+  } else {
+    print('$numberToCheck is Odd (Ganjil)');
+  }
+ }
