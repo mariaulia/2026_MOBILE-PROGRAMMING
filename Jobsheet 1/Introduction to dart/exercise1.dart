fc447bd0c379bc28d1671854f2d4e817a6209167
Maria Aulia <72290963+mariaulia@users.noreply.github.com>
2026-03-24 17:33:56 +0700

    Create exercise1.dart

---
+++ Jobsheet 1/Introduction to dart/exercise1.dart
@@ -0,0 +1,11 @@
+ void main() {
+  double num1 = 15;
+  double num2 = 4;
+
+  print('Angka yang digunakan: $num1 dan $num2');
+  print('Addition (Penjumlahan): ${num1 + num2}');
+  print('Subtraction (Pengurangan): ${num1 - num2}');
+  print('Multiplication (Perkalian): ${num1 * num2}');
+  print('Division (Pembagian): ${num1 / num2}');
+   
+ }
