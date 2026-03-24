da5d534fda23f5fe118d80c84badba11a2c1f461
Maria Aulia <72290963+mariaulia@users.noreply.github.com>
2026-03-24 17:22:55 +0700

    Create guidedpractice.dart

---
+++ Jobsheet 1/Introduction to dart/guidedpractice.dart
@@ -0,0 +1,9 @@
+var name = 'Maria Immaculata Aulia Kurnia W';
+var studenID = '4.33.24.1.13';
+var major = 'Computer Engineering';
+
+void main() {
+  print('Name: $name');
+  print('Student ID: $studenID');
+  print('Major: $major');
+}
